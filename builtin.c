7f331e926c2dac70d00e32bd9929bc3a94c7bc55
Anthony <75227567+Ablaze789@users.noreply.github.com>
2022-12-21 12:53:22 +0100

    Update

--- builtin.c
+++ builtin.c
@@ -76,16 +76,11 @@ int _mycd(info_t *info)
 		chdir_ret = chdir(info->argv[1]);
 
 	if (chdir_ret == -1)
-	{
 		print_error(info, "can't cd to ");
 		_eputs(info->argv[1]), _eputchar('\n');
-	}
 	else
-	{
 		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
 		_setenv(info, "PWD", getcwd(buffer, 1024));
-	}
-
 	return (0);
 }
 
